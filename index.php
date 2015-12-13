5a7e6f6cbce34a84a48a0ba69995b472446e28c1
andrew <cccp.world@gmail.com>
2015-12-14 05:03:54 +0700

    Правки адаптивного отображения главного слайдера и его элементов.

--- index.php
+++ index.php
@@ -11,35 +11,35 @@
 			<div class="item chiken">
 				<img src="/dist/images/chiken.png" alt="Курочка">
 				<div class="carousel-caption">
-				<p class="event">
-					с 30 июня по 6 июля 2015 года
-				</p>
-				<h3 class="title">
-					Окорочка куриные<br> охлажденные
-				</h3>
-				<p class="details">
-					1 кг
-				</p>
+					<p class="event">
+						с 30 июня по 6 июля 2015 года
+					</p>
+					<h3 class="title">
+						Окорочка куриные<br> охлажденные
+					</h3>
+					<p class="details">
+						1 кг
+					</p>
 				</div>
-				<div class="priceTag spriteSmith wow fadeIn" data-wow-delay=".382s">
-					<div class="icon-priceTag">
-						<div class="price">
-							<div class="overflow">
-								129 <sup>00</sup>
-							</div>
-						</div>
-						<div class="oldPrice">
-							<div class="overflow">
-								<div class="inline-block">
-									154 <sup>80</sup>
-									<strike class="wow fadeIn" data-wow-delay="1.618s"></strike>
-								</div>
-							</div>
-						</div>
-						<div class="discount wow pulse" data-wow-delay="2.382s">
-							-20%
+			</div>
+		</div>
+		<div class="priceTag spriteSmith wow fadeIn" data-wow-delay=".382s">
+			<div class="icon-priceTag">
+				<div class="price">
+					<div class="overflow">
+						129 <sup>00</sup>
+					</div>
+				</div>
+				<div class="oldPrice">
+					<div class="overflow">
+						<div class="inline-block">
+							154 <sup>80</sup>
+							<strike class="wow fadeIn" data-wow-delay="1.618s"></strike>
 						</div>
 					</div>
+				</div>
+				<div class="discount wow pulse" data-wow-delay="2.382s">
+					-20%
 				</div>
 			</div>
 		</div>
