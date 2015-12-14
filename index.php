aac2e48f580aa761218bc8a05e567a952a69070d
andrew <cccp.world@gmail.com>
2015-12-14 21:19:52 +0700

    Верстка блоков: список продуктов, летопись.

--- index.php
+++ index.php
@@ -55,5 +55,164 @@
 			</a>
 		</div>
 	</div>
+	<div class="promoList">
+		<div class="row">
+			<div class="col-xs-7">
+				<h3 class="title">
+					Наши рецепты
+				</h3>
+			</div>
+			<div class="col-xs-5">
+				<div class="titleCounter">
+					340 рецептов
+				</div>
+			</div>
+		</div>
+		<div class="row list">
+			<div class="col-sm-6 col-md-4">
+				<form class="item" novalidate>
+					<a href="#">
+						<div class="image">
+							<div class="paddingTop"></div>
+							<div class="absolute">
+								<div class="overflow">
+									<div class="cell">
+										<img src="/dist/images/pancakesWithStraberries.png" alt="Блинчики с клубникой">
+									</div>
+								</div>
+							</div>
+							<div class="priceTag">
+								<div class="cell">
+									<div class="overflow">
+										160<sup>00</sup>
+									</div>
+								</div>
+							</div>
+						</div>
+						<span class="infoBlock">
+							<h5 class="category spriteSmith">
+								<div class="icon icon-leaflet"></div> вегетарианский, простой
+							</h5>
+							<h4 class="name">
+								Блинчики с клубникой
+							</h4>
+							<span class="time spriteSmith">
+								<span class="icon icon-clock-lg visible-xs-inline-block"></span><span class="icon icon-clock visible-lg-inline-block"></span><span class="icon icon-clock-sm visible-md-inline-block"></span> 0:40
+							</span>
+						</span>
+						<div class="purchase">
+							<div class="cell">
+								Подобрать продукты
+							</div>
+						</div>
+					</a>
+				</form>
+			</div>
+			<!-- <div class="col-sm-6 col-md-4">
+				<form class="item" novalidate>
+					<a href="#">
+						<div class="image">
+							<div class="paddingTop"></div>
+							<div class="absolute">
+								<div class="overflow">
+									<div class="cell">
+										<img src="/dist/images/beafSteakWithPestoAndCorn.png" alt="Говяжий стейк с песто и кукурузой">
+									</div>
+								</div>
+							</div>
+							<div class="priceTag">
+								<div class="cell">
+									<div class="overflow">
+										190<sup>00</sup>
+									</div>
+								</div>
+							</div>
+						</div>
+						<span class="infoBlock">
+							<h5 class="category spriteSmith">
+								<div class="icon icon-burger"></div> сытный, сложный
+							</h5>
+							<h4 class="name">
+								Говяжий стейк с песто и кукурузой
+							</h4>
+							<span class="time spriteSmith">
+								<span class="icon icon-clock-lg visible xs"></span><span class="icon icon-clock visible-lg"></span><span class="icon icon-clock-sm visible-md"></span> 0:40
+							</span>
+						</span>
+						<div class="purchase">
+							<div class="cell">
+								Подобрать продукты
+							</div>
+						</div>
+					</a>
+				</form>
+			</div>
+			<div class="col-sm-6 col-md-4">
+				<form class="item" novalidate>
+					<a href="#">
+						<div class="image">
+							<div class="paddingTop"></div>
+							<div class="absolute">
+								<div class="overflow">
+									<div class="cell">
+										<img src="/dist/images/wafers.png" alt="Вафли с мёдом">
+									</div>
+								</div>
+							</div>
+							<div class="priceTag">
+								<div class="cell">
+									<div class="overflow">
+										230<sup>00</sup>
+									</div>
+								</div>
+							</div>
+						</div>
+						<span class="infoBlock">
+							<h5 class="category spriteSmith">
+								<div class="icon icon-egg"></div> сытный, сложный
+							</h5>
+							<h4 class="name">
+								Вафли с мёдом
+							</h4>
+							<span class="time spriteSmith">
+								<span class="icon icon-clock-lg visible xs"></span><span class="icon icon-clock visible-lg"></span><span class="icon icon-clock-sm visible-md"></span> 0:40
+							</span>
+						</span>
+						<div class="purchase">
+							<div class="cell">
+								Подобрать продукты
+							</div>
+						</div>
+					</a>
+				</form>
+			</div> -->
+		</div>
+	</div>
+	<div class="chronicle">
+		<div class="row">
+			<div class="col-xs-4 col-md-2 hidden-xs hidden-sm">
+				<div class="item"></div>
+			</div>
+			<div class="col-xs-4 col-md-2 hidden-xs hidden-sm">
+				<div class="item"></div>
+			</div>
+			<div class="col-xs-4 col-md-2 hidden-xs hidden-sm">
+				<div class="item"></div>
+			</div>
+			<div class="col-xs-4 col-md-2">
+				<div class="item"></div>
+			</div>
+			<div class="col-xs-4 col-md-2">
+				<div class="item"></div>
+			</div>
+			<div class="col-xs-4 col-md-2">
+				<div class="item">
+					<div class="cell">
+						<span>+</span>456
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
 <?php include('footer.php'); ?>
